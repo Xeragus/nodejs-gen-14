906f891c7c0a8e2de27c197eea36dde09dc64bd7
Xeragus <bobansugareski@gmail.com>
2021-09-20 21:40:37 +0200

    Add code for lecture 7

--- project/app.js
+++ project/app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
