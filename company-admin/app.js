bc59afe0743e79c4e525001911ffbb3acd80a8af
Xeragus <bobansugareski@gmail.com>
2021-10-08 20:58:25 +0200

    Add first part for lecture 13

--- company-admin/app.js
+++ company-admin/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
-var employeesRouter = require('./routes/employees');
+const indexRouter = require('./routes/index');
+const employeesRouter = require('./routes/employees');
+const jobTitlesRouter = require('./routes/job-titles');
 
-var app = express();
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/company-admin');
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/employees', employeesRouter);
+app.use('/job-titles', jobTitlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
